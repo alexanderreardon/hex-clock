93c4c0b9bafe667e481baf072a9e9d7216234549
Alex <alex.reardon14@gmail.com>
2019-04-29 21:18:19 -0400

    Added Google Analytics tracking among other changes.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var clock = document.getElementById('clock');
 var hexcode = document.getElementById('hexcode');
+var normalTime = document.getElementById('normal');
+var militaryTime = document.getElementById('military');
+
+
+function getFormat(el) {
+    if (el.className == "active") {
+        return;
+    } else {
+        var toggles = document.getElementById('toggle-time').children;
+        for (var i = 0; i < toggles.length; i++) {
+            toggles[i].className = "not-active"
+        }
+        el.className = "active";
+    }
+}
 
 function hexClock() {
     var time = new Date();
@@ -7,9 +22,13 @@ function hexClock() {
     var minutes = time.getMinutes().toString();
     var seconds = time.getSeconds().toString();
 
-    if (hours > 12) {
-        hours = (hours % 12).toString();
-    }
+    if (normalTime.className == "active") {
+        if (hours > 12) {
+            hours = (hours % 12).toString();
+        }
+    } else if (militaryTime.className == "active") {
+        hours = hours.toString();
+    };
 
     hours.length < 2 ? hours = '0' + hours : hours;
     minutes.length < 2 ? minutes = '0' + minutes : minutes;
